ebcd1649184791007210cffe518c34e233101128
qinxian1989 <qin__xuan@yeah.net>
2021-08-24 15:30:27 +0800

    update.gitignore

---
+++ python/kivy学习/ch3图形绘制/3.4旋转、平移和缩放坐标空间/1.旋转坐标空间/main.py
@@ -0,0 +1,13 @@
+﻿from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+
+class RotateGridLayoutWidght(GridLayout):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+
+class RotateApp(App):
+    def build(self):
+        return RotateGridLayoutWidght()
+
+if __name__ =='__main__':
+    RotateApp().run()
