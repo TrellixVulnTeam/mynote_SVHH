b8bc19e6e6d389f19872c3ee2c252eece981ffbc
qinxian1989 <qin__xuan@yeah.net>
2021-08-28 21:38:46 +0800

    更新kivy笔记

---
+++ python/kivy学习/ch4简单UX部件/4.1Button按钮/3.触发事件/main.py
@@ -0,0 +1,20 @@
+﻿from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+
+class ButtonFloatLayout(FloatLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def press_button(self,arg):
+        print('press_button is running%s'%arg)
+
+    def release_button(self):
+        print('release_button is running')
+
+class ButtonApp(App):
+    def build(self):
+        return ButtonFloatLayout()
+
+
+if __name__ == '__main__':
+    ButtonApp().run()
